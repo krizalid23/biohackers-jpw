48a28eb92a065642444dbd0a71726a036a4662f1
jp.waller <juanpablowaller76@gmail.com>
2021-06-10 22:13:41 -0300

    cuarto commit

--- Dropbox/Documents/biohackers-waller/biohackers-waller/src/App.js
+++ Dropbox/Documents/biohackers-waller/biohackers-waller/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer';
+import Counter from './components/CounterGeneral';
 import './App.css';
+
 
 export default function App() {
 
@@ -13,11 +15,13 @@ export default function App() {
     <div className="App">
       <Navbar />
       <div className='row'>
-      <img src={cambot1} id='cambot'/>
+      <img src={cambot1} id='cambot' title='' alt='' />
         <h3>BIENVENIDOS A LA PRIMERA TIENDA DE PRODUCTOS BIOTECNOLOGICOS DE ARGENTINA 2045</h3>
-        <img src={cambot2} id='cambot'/>
+        <img src={cambot2} id='cambot' title='' alt='' />
       </div>
       <ItemListContainer />
+      <br></br>
+      <Counter/>
     </div>
   );
 }
