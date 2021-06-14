6064b5e11e38042da221836ec058d052e50ad28f
jp.waller <juanpablowaller76@gmail.com>
2021-06-13 23:40:46 -0300

    quinto commit

---
+++ Dropbox/Documents/Downloads/Desktop/biohackers-waller/src/components/Navbar/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import CartWidget from './CartWidget';
+
+const NavbarStyle = {
+    backgroundColor: 'rgba(19, 20, 18, 0.507)',
+    width: '100%',
+    height: '0%',
+}
+
+const BrandingStyle = {
+    display: "flex",
+    flexDirection: "row",
+}
+
+const TituloYSubtituloStyle = {
+    display: "flex",
+    flexDirection: "column",
+}
+
+const MenuStyle = {
+    display: "grid",
+    gridTemplateColumns: 'repeat(5,auto)',
+    gridGap: '20px',
+    textAlign: 'center',
+    listStyleType: 'none',
+}
+
+
+export default function Navbar() {
+
+    var image1 = require('./../../img/chicavr.gif');
+
+    return (
+        <nav className="navbarItems" style={NavbarStyle}>
+            <div style={BrandingStyle}>
+                <img src={image1} className="logo" title='' alt='' />
+                <div style={TituloYSubtituloStyle}>
+                    <h1>BIOHACKERS</h1>
+                    <h2 className='subtitulo'>E-COMMERCE TRANSHUMANISTA</h2>
+                </div>
+                <div className='margins'>
+                    <ul style={MenuStyle}>
+                        <li className="nav-item">HOME</li>
+                        <li className="nav-item">PRODUCTOS</li>
+                        <li className="nav-item">CARRITO<CartWidget/></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+
+}
+
